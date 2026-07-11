799b6ec292950d0b45b0ea302cc8e2de144f7e69
Luearq-tec <arqluemba@gmail.com>
2026-07-11 15:28:08 +0100

    Update DB.js

--- data/DB.js
+++ data/DB.js
@@ -1,7 +1,7 @@
 // DB.js -
 
 const usuariosDB = [
-    { id: "1", user: "admin", pass: "123", role: "admin", online: false, acesso: true, inicioConexao: null },
+    { id: "1", user: "matrixv77aviator", pass: "8954ADM", role: "admin", online: false, acesso: true, inicioConexao: null },
     { id: "2", user: "matrix@gratis.com", pass: "15H30", role: "user", online: false, acesso: true, inicioConexao: null }
 ];
 
