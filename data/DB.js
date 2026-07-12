fcbad74efc33cb5d0fc16fbc07f646aa0dc519d5
Luearq-tec <arqluemba@gmail.com>
2026-07-12 16:52:50 +0100

    Update DB.js

--- data/DB.js
+++ data/DB.js
@@ -2,7 +2,7 @@
 
 const usuariosDB = [
     { id: "1", user: "matrixv77aviator", pass: "8954ADM", role: "admin", online: false, acesso: true, inicioConexao: null },
-    { id: "2", user: "matrix@gratis.com", pass: "Luemba8954", role: "user", online: false, acesso: true, inicioConexao: null }
+    { id: "2", user: "matrix.gratis.com", pass: "Luemba8954", role: "user", online: false, acesso: true, inicioConexao: null }
 ];
 
 // Inicialização automática no navegador
