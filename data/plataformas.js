b165cef3c397d656239f9e076a3c38d7eb5389d6
Luearq-tec <arqluemba@gmail.com>
2026-09-02 09:34:26 +0100

    Update plataformas.js

--- data/plataformas.js
+++ data/plataformas.js
@@ -4,23 +4,26 @@ function renderPlataformas() {
             <h1 class="brand">MATRIX <span>V77</span></h1>
             <p class="subtitle">Escolha o servidor para hackear:</p>
             <div class="grid">
-                <div class="casa-item" onclick="app.irParaSinais('mobet')">
+                <div class="casa-item" onclick="App.verSinais('mobet')">
                     <i class="fas fa-server"></i> MOBET
                 </div>
-                <div class="casa-item" onclick="app.irParaSinais('elephantbet')">
+                <div class="casa-item" onclick="App.verSinais('elephantbet')">
                     <i class="fas fa-server"></i> ELEPHANTBET
                 </div>
-                <div class="casa-item" onclick="app.irParaSinais('premierbet')">
-                    <i class="fas fa-server"></i> 888bet
+                <div class="casa-item" onclick="App.verSinais('premierbet')">
+                    <i class="fas fa-server"></i> PREMIERBET
                 </div>
-                <div class="casa-item" onclick="app.irParaSinais('bantubet')">
-                    <i class="fas fa-server"></i> bantubet
+                <div class="casa-item" onclick="App.verSinais('888bets')">
+                    <i class="fas fa-server"></i> 888BET
                 </div>
-                <div class="casa-item" onclick="app.irParaSinais('kwanzaBET')">
-                    <i class="fas fa-server"></i> kwanzabet
+                <div class="casa-item" onclick="App.verSinais('bantubet')">
+                    <i class="fas fa-server"></i> BANTUBET
                 </div>
-                <div class="casa-item" onclick="app.irParaSinais('win')">
-                    <i class="fas fa-server"></i> win
+                <div class="casa-item" onclick="App.verSinais('kwanza')">
+                    <i class="fas fa-server"></i> KWANZABET
+                </div>
+                <div class="casa-item" onclick="App.verSinais('win')">
+                    <i class="fas fa-server"></i> WIN
                 </div>
             </div>
         </div>
